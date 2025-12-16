1b79ea0b77c05aca6303f50383bd40485cc1e62b
kn2rf28k9g-cpu <jonas.gasparini@outlook.de>
2025-12-16 13:06:04 +0100

    Test

--- Kamera.py
+++ Kamera.py
@@ -1,7 +1,7 @@
-import subprocess
 from picamera2 import Picamera2
 from datetime import datetime
 import time
+from CNN import erkenne_muell
 
 picam2 = Picamera2()
 still_config = picam2.create_still_configuration()
@@ -11,9 +11,11 @@ picam2.start()
 time.sleep(2)
 
 Uhrzeit = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-filename = f"still_{Uhrzeit}.jpg"
+filename = f"still_{Uhrzeit}. jpg"
 
 picam2.capture_file(filename)
 picam2.stop()
 
-subprocess.run(["python3.11", "CNN.py", filename])
+kategorie, konfidenz = erkenne_muell(filename)
+print(f"Erkannt:  {kategorie}")
+print(f"Konfidenz: {konfidenz * 100:.2f}%")
